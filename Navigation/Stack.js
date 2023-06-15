c84b6e34c671546dac81303317ee6fa6c3961401
Sam <syakubov101@gmail.com>
2023-06-15 13:41:06 -0400

    removed Categories.js

--- Navigation/Stack.js
+++ Navigation/Stack.js
@@ -5,7 +5,6 @@ import React from 'react';
 import MainContainer from './MainContainer';
 import PostDetails from './Screens/PostDetails';
 import Chatbox from './Screens/ChatBox';
-import Categories from './Screens/Categories';
 import ViewProfile from './Screens/ViewProfile';
 import EditPost from './Screens/EditPosts';
 import ViewImages from './Screens/ImageViewer';
@@ -30,7 +29,6 @@ const Stack = () => {
       <Stack.Screen name = "Sign Up" component = {SignUp}/>
       <Stack.Screen name = "post details" component = {PostDetails}/>
       <Stack.Screen name = "chat box" component = {Chatbox} />
-      <Stack.Screen name = "categories" component = {Categories} />
       <Stack.Screen name = "view profile" component = {ViewProfile} />
       <Stack.Screen name = "Edit Post" component = {EditPost} />
       <Stack.Screen name = "Image Viewer" component = {ViewImages} />
