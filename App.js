82e8ec455412c84d5662156f825a3816d496f090
syakub-py <75036399+syakub-py@users.noreply.github.com>
2023-09-25 15:30:13 -0400

    moved app.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import Stack from "./Stack";
+import Stack from "./src/Stack";
 
 export default function App() {
   return (
