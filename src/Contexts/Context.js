cacee752a90f3b79c5ebee200086b0345554e8f3
Nyle <syakubov101@gmail.com>
2023-11-15 20:12:18 -0500

    added context file

---
+++ src/Contexts/Context.js
@@ -0,0 +1,25 @@
+class PostContext {
+  constructor() {
+    this.postedBy = '';
+    this.sqft = '';
+    this.usd = '';
+    this.bathrooms = '';
+    this.bedrooms = '';
+    this.category = '';
+    this.coordinates = {
+      latitude: 0,
+      longitude: 0,
+    };
+    this.currency = [];
+    this.date = '';
+    this.description = '';
+    this.id = 78;
+    this.likes = [];
+    this.pic = [];
+    this.profilePic = '';
+    this.sold = '';
+    this.title = '';
+    this.views = 206;
+  }
+}
+
